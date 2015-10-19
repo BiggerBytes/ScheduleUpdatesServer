a73ce6be3a916c3351a0df9c1d5523672d227984
Avishay <avikillu@gmail.com>
2015-10-19 13:41:48 +0300

    Dummies, fixes.

--- src/com/olympicat/scheduleupdates/Main.java
+++ src/com/olympicat/scheduleupdates/Main.java
@@ -18,7 +18,7 @@ import java.util.logging.Level;
  */
 public class Main {
     private static List<ScheduleChange> changeArr = null;
-    private static final Long REFRESH_DELAY = (1l*60l*60l*1000l); //    =   1h in milliseconds
+    private static final Long REFRESH_DELAY = (30l*60l*1000l); //    =   30m in milliseconds
 
     public static void main(String[] args) throws IOException {
         java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
